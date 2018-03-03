6816fc343679adf758385764c4cab26c7372daff
Ebrahim Ali <ebrahimali192@yahoo.com>
2018-03-04 00:36:43 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$('#logmein').click(function(){
+    $('#loginModal').modal('show');
+});
+
+$('#reserveme').click(function(){
+    $('#modalReservation').modal('show');
+});
+
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval: 2000});
+    $("#carousel-button").click(function(){
+        if($("#carousel-button").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass("fa-pause");
+            $("#carousel-button").children("span").addClass("fa-play");
+        }
+        else if($("#carousel-button").children("span").hasClass("fa-play")){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass("fa-play");
+            $("#carousel-button").children("span").addClass("fa-pause");
+        }
+    });
+});
